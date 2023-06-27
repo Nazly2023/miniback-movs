c21f39f4ee79a3751319d44c436add7920932827
Nazly Zambrano <nazmi.016@gmail.com>
2023-06-27 12:30:46 -0500

    Arreglo CORS

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(`${__dirname}/data/db.json`);
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5500");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
+
+server.use(cors());
 
 server.use(router);
 server.use(middlewares);
